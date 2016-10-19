9bb02aedf25d376b910a9efcf33c18f2135cbc6e
Faisal <faisal.al.tameemi@gmail.com>
2016-10-19 11:05:35 -0400

    lecture notes

--- public/js/index.script.js
+++ public/js/index.script.js
@@ -1,95 +1,86 @@
 'use strict';
 
-(($) => {
-	$(() => {
+$(document).ready(() => {
 
 		const ROOT_URL = 'http://jsonplaceholder.typicode.com';
+		let article_load_counter = 0;
 
-		// reusable handle error function
-		// const handleError = (label) => {
-		// 	return (err) => {
-		// 			console.debug(`Error @ ${label}`, err);
-		// 	}
+		const handleError = (method) => {
+			return (err) => {
+				console.debug(method, err);
+			}
+		}
+
+		// const handleLoadCommentsError = (err) => {
+		// 	console.debug('loadComments', err)
 		// }
-
-		// return an HTML element (as a string) from an article object
-		// const buildArticleHTML = (article) => {
-		// 	return `
-		// 		<div class="post-preview" data-article-id="${article.id}">
-		// 				<a href="#">
-		// 						<h2 class="post-title">
-		// 								${article.title}
-		// 						</h2>
-		// 						<h3 class="post-subtitle">
-		// 								Problems look mighty small from 150 miles up
-		// 						</h3>
-		// 				</a>
-		// 				<p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 24, 2014</p>
-		// 		</div>
-		// 		<hr>
-		// 	`;
-		// }
-
-		// load a single article via AJAX `GET` request
-		// const loadArticle = (article_id, _onLoaded) => {
-		// 	$.ajax({
-		// 			url: `${ROOT_URL}/posts/${article_id}`,
-		// 			method: 'GET'
-		// 		})
-		// 		.then((article) => {
-		// 			console.log(article);
-		// 			return article;
-		// 		})
-		// 		.then((article) => {
-		// 			// if there's a callback, invoke it
-		// 			if(_onLoaded) _onLoaded(article);
-		// 			return false;
-		// 		})
-		// 		.fail(handleError('loadArticle'));
-		// }
-
-		// load all articles via AJAX `GET` request, then render them on page
-		// const loadArticles = () => {
-		// 	// an inner function to render all the articles from JSON data
-		// 	const renderArticles = (articles) => {
-		// 		// fetch the HTML element where we will render articles using jQuery
-		// 		const articles_elm = $('#articles');
-		// 		// append each article into the element
-		// 		articles.forEach((article) => {
-		// 			articles_elm.append(buildArticleHTML(article));
-		// 		})
-		// 	}
 		//
-		// 	// make the AJAX call to load all the articles from the API
-		// 	$.get(`${ROOT_URL}/posts`)
-		// 		.then(renderArticles)
-		// 		.fail(handleError('loadArticles'));
+		// const handleLoadArticlesError = (err) => {
+		// 	console.debug('loadArticles', err)
 		// }
 
-		// load all comments for a given `article_id`
-		// const loadArticleComments = (article_id) => {
-		// 	$.get(`${ROOT_URL}/posts/${article_id}/coments`)
-		// 		.then((comments) => {
-		// 			console.log(comments);
-		// 		})
-		// 		.fail(handleError('loadArticleComments'));
-		// }
+		/**
+		@description: loads comments data for a specific post from JSON api
+		*/
+		const loadComments = (post_id, _cb) => {
+			$.ajax({
+				method: 'GET',
+				url: `${ROOT_URL}/posts/${post_id}/comments`,
+				success: (response) => {
+					_cb(response);
+				},
+				fail: handleError('loadComments')
+			});
+		}
 
-		// create a click listener for any article preview
-		// $('#articles').on('click', '.post-preview > a', (ev) => {
-		//   ev.preventDefault();
-		// 	const article_id = $(this).parent().attr('data-article-id');
-		// 	console.log(`Article #${article_id} has been clicked.`);
-		// });
+		/**
+		@description: loads articles data from JSON api
+		*/
+		const loadFeedData = (_cb) => {
+			$.ajax({
+				method: 'GET',
+				url: `${ROOT_URL}/posts/`,
+				// data: {
+				// 	skip: article_load_counter,
+				// 	limit: 50
+				// },
+				success: (response) => {
+					// increment counter with count of articles loaded
+					article_load_counter += response.length;
+					_cb(response);
+				},
+				fail: handleError('loadFeedData')
+			});
+		}
 
-		// load more button click listener
-		// $('#load-more').on('click', (ev) => {
-		//   ev.preventDefault();
-		// 	loadArticles();
-		// });
+		/**
+		@description: Given data of articles, converts the objects into HTML strings
+									and appends them onto the page.
+		*/
+		const renderArticles = (data) => {
+			const html_articles = data.map((current_article) => {
+				return `<div class="post-preview" data-article-id="${current_article.id}">
+						<a href="${ROOT_URL}/posts/${current_article.id}">
+								<h2 class="post-title">
+										${current_article.title}
+								</h2>
+								<h3 class="post-subtitle">
+										${current_article.body.slice(0, 50)}
+								</h3>
+						</a>
+						<p class="post-meta">Posted by <a href="#">Jane Doe</a> on January 20, 2017</p>
+				</div>
+				<hr>`;
+			});
+			$('#articles').append(html_articles.join(''));
+		}
+
+		// load more data listener
+		$('#load-more').on('click', (ev) => {
+			ev.preventDefault();
+			loadFeedData(renderArticles);
+		})
 
 		// on page load
-		// loadArticles();
-		
-	});
+		loadFeedData(renderArticles);
 });
