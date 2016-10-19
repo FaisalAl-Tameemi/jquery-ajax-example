0dfbcb512f90686932ecd86b33b5a275f11ddb37
Faisal <faisal.al.tameemi@gmail.com>
2016-10-18 20:04:17 -0400

    edits

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 'use strict';
 
-let express = require("express"),
-    app = express(),
-    bodyParser = require('body-parser'),
-    errorHandler = require('errorhandler'),
-    methodOverride = require('method-override'),
-    hostname = process.env.HOSTNAME || 'localhost',
-    port = parseInt(process.env.PORT, 10) || 5000,
-    publicDir = process.argv[2] || __dirname + '/public',
-    path = require('path');
+const express = require("express"),
+      app = express(),
+      bodyParser = require('body-parser'),
+      errorHandler = require('errorhandler'),
+      methodOverride = require('method-override'),
+      hostname = process.env.HOSTNAME || 'localhost',
+      port = parseInt(process.env.PORT, 10) || 5000,
+      publicDir = process.argv[2] || __dirname + '/public',
+      path = require('path');
 
 /*
 	Define the routes below
